404119dc63df4bbb0f67ff33d14df4c3b6b002a7
Bharath-007 <bhbharath8@gmail.com>
2023-02-09 17:04:57 +0530

    feat: TS Basics

---
+++ Milestone1/output/1.TSBasics.js
@@ -0,0 +1,7 @@
+"use strict";
+//Primitive type - number,string, boolean ,bigint,null,undefined,symbol
+let stuName = 'Bharath'; //the ts file will take it is of type:String
+let employeeName; //delare variable employeeName of type String
+employeeName = 'Arun'; //assign variable employeeName
+let isPermanent = true; //defining the variable with type boolean
+let age = 21;
---
+++ Milestone1/src/1.TSBasics.ts
@@ -0,0 +1,9 @@
+//Primitive type - number,string, boolean ,bigint,null,undefined,symbol
+let stuName = 'Bharath' //the ts file will take it is of type:String
+
+let employeeName:string; //delare variable employeeName of type String
+employeeName = 'Arun'//assign variable employeeName
+let isPermanent:boolean = true; //defining the variable with type boolean
+
+let age = 21;
+ 
