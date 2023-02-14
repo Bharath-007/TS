09a0d6085e421dd9bbbf7bc0f71d3ea816918f81
Bharath-007 <bhbharath8@gmail.com>
2023-02-14 10:02:27 +0530

    feat: Milestone 3 Completed

--- Milestone3/typeManipulation.ts
+++ Milestone3/typeManipulation.ts
@@ -1,19 +1,19 @@
 //keyof 
 type staff = {
     name: string;
-    salary:number;
+    salary: number;
 }
 
-type staffkeys  = keyof staff;//now staffKeys inclulde union of staff properties (name | salary)
+type staffkeys = keyof staff;//now staffKeys inclulde union of staff properties (name | salary)
 
 //keyof in generics
-const getProperty = <T,K extends keyof T>(prop:T,key:K):T[K] => {
+const getProperty = <T, K extends keyof T>(prop: T, key: K): T[K] => {
     return prop[key];
 }
 
-const developer:staff={
-    name:"Developer",
-    salary:10000
+const developer: staff = {
+    name: "Developer",
+    salary: 10000
 };
 
 const nametype = getProperty(developer, "name");
@@ -25,22 +25,24 @@ console.log(nametype);
     that can be accessed by keyof type operator if we access other properties other than developer property it will be a compile time error
 */
 
+
 //typeof generic
-const getType = <T>(arg:T):string => {
+const getType = <T>(arg: T): string => {
     return typeof arg;
-} 
+}
+
 console.log(getType('Hello world!'));
 console.log(getType(123));
 console.log(getType(false));
 
 //ReturnType utility function in TS - used to extract the return type of the function
-const getID = (id:number):number => {
+const getID = (id: number): number => {
     return id;
 }
 
 type IdType = ReturnType<typeof getID>;
 
-const myID:IdType = 5;
+const myID: IdType = 5;
 
 /*
 the ReturnType utility function in TS - used to extract the return type of the function
@@ -49,52 +51,53 @@ of the getID function which is number and given the type to another varible myID
 
 
 //Indexed Access Types
-type MobileInfo ={
-    ram:number;
-    phone:string;
- }
- type Ram = MobileInfo['ram'];
- const myRam:Ram = 8;
+type MobileInfo = {
+    ram: number;
+    phone: string;
+}
 
- /*
- In this indexed access type we are assigning the type Ram using the indexes of MobileInfo
- and giving that as type to myRam var and assigning it to number 8(8GB);
- */
+type Ram = MobileInfo['ram'];
+const myRam: Ram = 8;
+
+/*
+In this indexed access type we are assigning the type Ram using the indexes of MobileInfo
+and giving that as type to myRam var and assigning it to number 8(8GB);
+*/
 
 
- //Conditional Types
- type ExtractId<T extends {id:string|number}> = T['id']
+//Conditional Types
+type ExtractId<T extends { id: string | number }> = T['id']
 
- interface NumericID{
-    id:number;
- }
+interface NumericID {
+    id: number;
+}
 
- interface StringID{
-    id:string;
- }
+interface StringID {
+    id: string;
+}
 
- type numericID = ExtractId<NumericID>;//numericID = number
- type stringID = ExtractId<StringID>;//stringID = string
+type numericID = ExtractId<NumericID>;//numericID = number
+type stringID = ExtractId<StringID>;//stringID = string
 
- //Mapped types
-type OnlyUser = {[key:string]:string|number}
+//Mapped types
+type OnlyUser = { [key: string]: string | number }
 
-const typeOnlyUser:OnlyUser = {
-        name:'bhaarth'
+const typeOnlyUser: OnlyUser = {
+    name: 'bhaarth'
 }
 
 //mapped type in generic - it uses union property
-type OptionFlag<T> = {[P in keyof T]:boolean} // here T is any type by  using the keyof type assigning then values  that can have union of T
+type OptionFlag<T> = { [P in keyof T]: boolean } // here T is any type by  using the keyof type assigning then values  that can have union of T
 
 
 //Template literal types
 type route_1 = {
-    stop1:'Hopes',
-    stop2:'FunMall'
+    stop1: 'Hopes',
+    stop2: 'FunMall'
 }
 type route_2 = {
-    stop1:'Gandhipuram',
-    stop2:'ukkadam'
+    stop1: 'Gandhipuram',
+    stop2: 'ukkadam'
 }
 type busStoppings = "route_1" | "route_2";
 
@@ -102,13 +105,13 @@ type printStoppings = `${busStoppings}_stop`
 
 
 //Class
-class Person{
+class Person {
     name: string;
 
-    constructor(name: string){
+    constructor(name: string) {
         this.name = name;
     }
-    greet(greet:string):string{
+    greet(greet: string): string {
         return `${greet} ${this.name}`
     }
 }
