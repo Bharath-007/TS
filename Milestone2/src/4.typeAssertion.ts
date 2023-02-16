7c9e1d45a916a632cff695764ecd06661a200195
Bharath-007 <bhbharath8@gmail.com>
2023-02-16 10:49:12 +0530

    feat: Milestone2 changes updated

--- Milestone2/src/4.typeAssertion.ts
+++ Milestone2/src/4.typeAssertion.ts
@@ -2,4 +2,8 @@
 
 let code: any = 123; 
 let employeeCode = <number> code; 
-console.log(typeof(employeeCode)); //Output: number
+console.log(typeof(employeeCode)); //Output: number'
+
+let employeeCode2 = {name:'Employ',empID:211}
+let empname = (employeeCode2 as {name:string}).name;
+console.log(empname);
